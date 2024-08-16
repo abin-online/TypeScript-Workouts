7ffc493eb3516867ded2dacafe0110ee7d419492
Abin Babu <abinbabuonline@gmail.com>
2024-08-16 18:52:46 +0530

    array done

---
+++ ARRAYS/arr1.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = []; //syntax for string array
+var heroPower = [];
+var allUser = [];
+allUser.push({ name: '', isActive: true });
+superHeros.push('spiderman');
+heroPower.push(4);
---
+++ ARRAYS/arr1.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = [] //syntax for string array
+const heroPower: Array <number> = []
+
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUser: User[] = []
+
+allUser.push({name: '' , isActive: true})
+
+superHeros.push('spiderman')
+
+heroPower.push(4)
+
+
+
+
+export {}
